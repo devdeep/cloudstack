b8904ed27cc7843c94ac30d06cccc8137b8ae438
Anshul Gangwar <anshul.gangwar@citrix.com>
2014-02-07 16:17:58 +0530

    CS-19171: fixed vmsync for hyperv 2

--- plugins/hypervisors/hyperv/src/com/cloud/hypervisor/hyperv/resource/HypervDirectConnectResource.java
+++ plugins/hypervisors/hyperv/src/com/cloud/hypervisor/hyperv/resource/HypervDirectConnectResource.java
@@ -62,6 +62,7 @@ import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import com.cloud.agent.api.Answer;
 import com.cloud.agent.api.CheckRouterAnswer;
@@ -132,6 +133,7 @@ import com.cloud.utils.StringUtils;
 import com.cloud.utils.net.NetUtils;
 import com.cloud.utils.ssh.SshHelper;
 import com.cloud.vm.VirtualMachine;
+import com.cloud.vm.VirtualMachine.PowerState;
 import com.cloud.vm.VirtualMachineName;
 /**
  * Implementation of dummy resource to be returned from discoverer.
@@ -177,6 +179,20 @@ public class HypervDirectConnectResource extends ServerResourceBase implements S
         return Type.Routing;
     }
 
+    protected static HashMap<String, PowerState> s_powerStatesTable;
+    static {
+        s_powerStatesTable = new HashMap<String, PowerState>();
+        s_powerStatesTable.put("DISABLED", PowerState.PowerOff);
+        s_powerStatesTable.put("PAUSED", PowerState.PowerOn);
+        s_powerStatesTable.put("ENABLED", PowerState.PowerOn);
+        s_powerStatesTable.put("SUSPENDED", PowerState.PowerOn);
+        s_powerStatesTable.put("UNKNOWN", PowerState.PowerUnknown);
+        s_powerStatesTable.put("STOPPING", PowerState.PowerOff);
+        s_powerStatesTable.put("PAUSING", PowerState.PowerOn);
+        s_powerStatesTable.put("RESUMING", PowerState.PowerOn);
+        s_powerStatesTable.put("SAVING", PowerState.PowerOn);
+    }
+
     @Override
     public final StartupCommand[] initialize() {
         // assert
@@ -206,6 +222,7 @@ public class HypervDirectConnectResource extends ServerResourceBase implements S
         defaultStartRoutCmd.setPrivateIpAddress(_agentIp);
         defaultStartRoutCmd.setStorageIpAddress(_agentIp);
         defaultStartRoutCmd.setPool(_clusterGuid);
+        defaultStartRoutCmd.setHostVmStateReport(getHostVmStateReport());
 
         s_logger.debug("Generated StartupRoutingCommand for _agentIp \""
                 + _agentIp + "\"");
@@ -329,7 +346,7 @@ public class HypervDirectConnectResource extends ServerResourceBase implements S
     @Override
     public final PingCommand getCurrentStatus(final long id) {
     	// TODO, need to report VM states on host
-        PingCommand pingCmd = new PingRoutingCommand(getType(), id, null, null);
+        PingCommand pingCmd = new PingRoutingCommand(getType(), id, null, getHostVmStateReport());
 
         if (s_logger.isDebugEnabled()) {
             s_logger.debug("Ping host " + _name + " (IP " + _agentIp + ")");
@@ -382,6 +399,61 @@ public class HypervDirectConnectResource extends ServerResourceBase implements S
             return result;
         }
         return null;
+    }
+
+    public final ArrayList<Map<String, String>> requestHostVmStateReport() {
+        // Set HTTP POST destination URI
+        // Using java.net.URI, see
+        // http://docs.oracle.com/javase/1.5.0/docs/api/java/net/URI.html
+        URI agentUri = null;
+        try {
+            String cmdName = "org.apache.cloudstack.HostVmStateReportCommand";
+            agentUri =
+                    new URI("https", null, _agentIp, _port,
+                            "/api/HypervResource/" + cmdName, null, null);
+        } catch (URISyntaxException e) {
+            // TODO add proper logging
+            String errMsg = "Could not generate URI for Hyper-V agent";
+            s_logger.error(errMsg, e);
+            return null;
+        }
+        String incomingCmd = postHttpRequest("{}", agentUri);
+
+        if (incomingCmd == null) {
+            return null;
+        }
+        ArrayList<Map<String, String>> result = null;
+        try {
+            result = s_gson.fromJson(incomingCmd, new TypeToken<ArrayList<HashMap<String, String>>>() {
+            }.getType());
+        } catch (Exception ex) {
+            String errMsg = "Failed to deserialize Command[] " + incomingCmd;
+            s_logger.error(errMsg, ex);
+        }
+        s_logger.debug("HostVmStateReportCommand received response "
+                + s_gson.toJson(result));
+        if (!result.isEmpty()) {
+            return result;
+        }
+        return null;
+    }
+
+    private static PowerState convertPowerState(String powerState) {
+        return s_powerStatesTable.get(powerState);
+    }
+
+    protected HashMap<String, HostVmStateReportEntry> getHostVmStateReport() {
+        final HashMap<String, HostVmStateReportEntry> vmStates = new HashMap<String, HostVmStateReportEntry>();
+        ArrayList<Map<String, String>> vmList = requestHostVmStateReport();
+        if (vmList == null) {
+            return null;
+        }
+
+        for (Map<String, String> vmMap : vmList) {
+            String name = (String)vmMap.keySet().toArray()[0];
+            vmStates.put(name, new HostVmStateReportEntry(convertPowerState(vmMap.get(name)), _guid, null));
+        }
+        return vmStates;
     }
 
     // TODO: Is it valid to return NULL, or should we throw on error?
