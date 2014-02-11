devdeep/cloudstack

commit d3fe7efb2931ecde641cf7b042016dc901467a43

CS-19006: Network Offering creation should not be allowed when Firewall and SourceNAT providers are different
restricting that both Firewall and SourceNat service providers should be
same while creating network offering

plugins/network-elements/juniper-srx/src/com/cloud/network/element/JuniperSRXExternalFirewallElement.java
@@ -533,6 +533,10 @@ PortForwardingServiceProvider, IpDeployer, JuniperSRXFirewallElementService, Sta
             s_logger.warn("SRX must be used as Firewall Service Provider in the network");
             return false;
         }
+        if (services.contains(Service.Firewall) ^ services.contains(Services.SourceNat)) {
+            s_logger.warn("SRX must be used as both Firewall and SourceNAT Service Provider in the network.");
+            return false;
+        }
         return true;
     }
 

server/src/com/cloud/network/element/VirtualRouterElement.java
@@ -1021,6 +1021,10 @@ public class VirtualRouterElement extends AdapterBase implements VirtualRouterEl
                 return false;
             }
         }
+        if ( services.contains(Service.SourceNat) ^ services.contains(Service.Firewall)) {
+            s_logger.warn("Virtual router can only enable both Firewall and SourceNat services together.");
+            return false;
+        }
         return true;
     }
 

commit 67c8df5bb7ce047f792d143e789ca62c2a073601

fix typo introduced in d3fe7efb2931ecde641cf7b042016dc901467a43

plugins/network-elements/juniper-srx/src/com/cloud/network/element/JuniperSRXExternalFirewallElement.java
@@ -533,7 +533,7 @@ PortForwardingServiceProvider, IpDeployer, JuniperSRXFirewallElementService, Sta
             s_logger.warn("SRX must be used as Firewall Service Provider in the network");
             return false;
         }
-        if (services.contains(Service.Firewall) ^ services.contains(Services.SourceNat)) {
+        if (services.contains(Service.Firewall) ^ services.contains(Service.SourceNat)) {
             s_logger.warn("SRX must be used as both Firewall and SourceNAT Service Provider in the network.");
             return false;
         }