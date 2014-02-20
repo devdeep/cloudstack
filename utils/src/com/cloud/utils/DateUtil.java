devdeep/cloudstack

commit e6909505a8a96cab6085a754bec243377ad5ae22

BUG-ID: CS-19196: pass InsufficientCapacityException across job boundary since some HA logic relies on it. Reviewed-By: Self

utils/src/com/cloud/utils/DateUtil.java
@@ -16,15 +16,12 @@
 // under the License.
 package com.cloud.utils;
 
-import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
-
-import javax.xml.crypto.Data;
 
 import com.cloud.utils.exception.CloudRuntimeException;
 
@@ -268,11 +265,6 @@ public class DateUtil {
 		TimeZone localTimezone = Calendar.getInstance().getTimeZone();
 		TimeZone gmtTimezone = TimeZone.getTimeZone("GMT");
 		TimeZone estTimezone = TimeZone.getTimeZone("EST");
-
-		Date time = new Date();
-
-        URL url = Date.class.getResource(Data.class.getName() + ".class");
-        System.out.println("Url: " + url.toString());
 
 		System.out.println("local time :" + getDateDisplayString(localTimezone, time));
 		System.out.println("GMT time   :" + getDateDisplayString(gmtTimezone, time));

engine/orchestration/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -748,8 +748,8 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
     }
 
     @Override
-    public void advanceStart(String vmUuid, Map<VirtualMachineProfile.Param, Object> params, DeploymentPlan planToDeploy, DeploymentPlanner planner) throws InsufficientCapacityException,
-            ConcurrentOperationException, ResourceUnavailableException {
+    public void advanceStart(String vmUuid, Map<VirtualMachineProfile.Param, Object> params, DeploymentPlan planToDeploy, DeploymentPlanner planner)
+            throws InsufficientCapacityException, ConcurrentOperationException, ResourceUnavailableException {
 
         AsyncJobExecutionContext jobContext = AsyncJobExecutionContext.getCurrentExecutionContext();
         if (!VmJobEnabled.value() || jobContext.isJobDispatchedBy(VmWorkConstants.VM_WORK_JOB_DISPATCHER)) {
@@ -782,6 +782,8 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
                     throw (ConcurrentOperationException)jobResult;
                 else if (jobResult instanceof ResourceUnavailableException)
                     throw (ResourceUnavailableException)jobResult;
+                else if (jobResult instanceof InsufficientCapacityException)
+                    throw (InsufficientCapacityException)jobResult;
                 else if (jobResult instanceof RuntimeException)
                     throw (RuntimeException)jobResult;
                 else if (jobResult instanceof Throwable)

commit fc559ed21fc74830f765a4b614409639e7bb3a39

BUG-ID: CS-19196: fix a compiling regression in commit e6909505a8a96cab6085a754bec243377ad5ae22. Reviewed-By: self

utils/src/com/cloud/utils/DateUtil.java
@@ -266,6 +266,8 @@ public class DateUtil {
 		TimeZone gmtTimezone = TimeZone.getTimeZone("GMT");
 		TimeZone estTimezone = TimeZone.getTimeZone("EST");
 
+        Date time = new Date();
+
 		System.out.println("local time :" + getDateDisplayString(localTimezone, time));
 		System.out.println("GMT time   :" + getDateDisplayString(gmtTimezone, time));
 		System.out.println("EST time   :" + getDateDisplayString(estTimezone, time));